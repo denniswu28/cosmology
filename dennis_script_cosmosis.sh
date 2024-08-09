64ae1bbf5442d3f07cc1ab24559dc7d9fbef93f1
denniswu28 <tw292@duke.edu>
2024-08-08 20:20:05 -0400

    minor fix in script

--- dennis_script_cosmosis.sh
+++ dennis_script_cosmosis.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l                     
-#SBATCH -N 2
-#SBATCH -n 100
+#SBATCH -N 1
+#SBATCH -n 1
 #SBATCH -t 48:00:00
 #SBATCH --account=cosmology
 #SBATCH --error=err/cosmosis_0.err 
 #SBATCH --mem=200G
-#SBATCH -p cosmology
+#SBATCH -p common
 #SBATCH --mail-type=all
 #SBATCH --mail-user=tianrui.wu@duke.edu
 
@@ -23,14 +23,20 @@ export OMP_NUM_THREADS=1
 # export SLURM_CPU_BIND=none
 # export SLURM_WHOLE=1
 
-mpirun -n 100 cosmosis --mpi /hpc/group/cosmology/denniswu/cosmosis_files/script/params_real.ini
-mpirun -n 100 cosmosis --mpi /hpc/group/cosmology/denniswu/cosmosis_files/script/params_real_2.ini
-mpirun -n 100 cosmosis --mpi /hpc/group/cosmology/denniswu/cosmosis_files/script/params_real_3.ini
+# mpirun -n 100 cosmosis --mpi /hpc/group/cosmology/denniswu/cosmosis_files/script/params_real.ini
+# mpirun -n 100 cosmosis --mpi /hpc/group/cosmology/denniswu/cosmosis_files/script/params_real_2.ini
+# mpirun -n 100 cosmosis --mpi /hpc/group/cosmology/denniswu/cosmosis_files/script/params_real_3.ini
 
 # cosmosis /hpc/group/cosmology/denniswu/cosmosis_files/script/params_pseudo.ini
 # cosmosis-postprocess denniswu/cosmosis_files/output/truth_truth_3x2pt/output.txt -o denniswu/cosmosis_files/output/truth_truth_3x2pt/plot -p truth_truth --derive denniswu/cosmosis_files/script/derived.py
 # cosmosis-postprocess denniswu/cosmosis_files/output/roman_roman_gw/output.txt -o denniswu/cosmosis_files/output/roman_roman_gw/plot -p roman_roman --derive denniswu/cosmosis_files/script/derived.py
 # cosmosis-postprocess denniswu/cosmosis_files/output/truth_truth_wl/output.txt denniswu/cosmosis_files/output/rubin_rubin_wl/output.txt denniswu/cosmosis_files/output/roman_roman_wl/output.txt -o denniswu/cosmosis_files/output/wl_3overlay/plot -p cosmic_shear --legend="truth|rubin|roman"
-# cosmosis-postprocess denniswu/cosmosis_files/output/roman_roman_3x2pt/output.txt -o denniswu/cosmosis_files/output/roman_roman_3x2pt/plot -p roman_roman --derive denniswu/cosmosis_files/script/derived.py
+cosmosis-postprocess denniswu/cosmosis_files/output/truth_roman_3x2pt/output.txt -o denniswu/cosmosis_files/output/truth_roman_3x2pt/plot -p truth_roman --derive denniswu/cosmosis_files/script/derived.py
+cosmosis-postprocess denniswu/cosmosis_files/output/truth_roman_wl/output.txt -o denniswu/cosmosis_files/output/truth_roman_wl/plot -p truth_roman --derive denniswu/cosmosis_files/script/derived.py
+cosmosis-postprocess denniswu/cosmosis_files/output/truth_roman_gw/output.txt -o denniswu/cosmosis_files/output/truth_roman_gw/plot -p truth_roman --derive denniswu/cosmosis_files/script/derived.py
+cosmosis-postprocess denniswu/cosmosis_files/output/truth_rubin_3x2pt/output.txt -o denniswu/cosmosis_files/output/truth_rubin_3x2pt/plot -p truth_rubin --derive denniswu/cosmosis_files/script/derived.py
+cosmosis-postprocess denniswu/cosmosis_files/output/truth_rubin_wl/output.txt -o denniswu/cosmosis_files/output/truth_rubin_wl/plot -p truth_rubin --derive denniswu/cosmosis_files/script/derived.py
+cosmosis-postprocess denniswu/cosmosis_files/output/truth_rubin_gw/output.txt -o denniswu/cosmosis_files/output/truth_rubin_gw/plot -p truth_rubin --derive denniswu/cosmosis_files/script/derived.py
+
 
 # cosmosis-postprocess --help
